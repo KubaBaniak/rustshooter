5a9a22a9d836483a88898c8325955b5e216eb864
KubaBaniak <kuba121201@gmail.com>
2023-04-04 21:04:46 +0200

    shooting, move validation, bullets on map

---
+++ README.md
@@ -0,0 +1,8 @@
+###**Todo:**
+
+[04.04.2023]
+- Shooting front, validation of moves, bullet management on board ✔
+
+- Shooting left, right, back
+
+- Resizable map (change static values - 8)
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,23 @@ use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use std::io::{Write, stdout, stdin, Stdout};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Position(u16, u16);
 
+#[derive(Debug)]
 struct ElementsOnMap {
     bullets: Vec<Bullet>,
+    player: Player,
 }
 
+#[derive(Debug)]
 struct Player {
     position: Position,
 }
 
+#[derive(Debug)]
 struct Bullet {
     position: Position,
-    speed: u16,
     direction: u16,
 }
 
@@ -26,15 +29,14 @@ impl Position {
         (self.0, self.1)
     }
 }
-
 impl Player {
     // 1 - up, 2 - right, 3 - down, 4 - left
     fn move_player(&mut self, direction: u16) {
-        match direction {
-            1 => self.position.0 -= 1, // x + 1
-            2 => self.position.1 += 1, // x + 1
-            3 => self.position.0 += 1, // x - 1
-            4 => self.position.1 -= 1, // y - 1
+        match (direction, &mut self.position) {
+            (1, Position(row, _)) if *row != 1 => *row -= 1, // x + 1
+            (2, Position(_, col)) if *col != 8 => *col += 1, // x + 1
+            (3, Position(row, _)) if *row != 8 => *row += 1, // x + 1
+            (4, Position(_, col)) if *col != 1 => *col -= 1, // x + 1
             _ => ()
         }
     }
@@ -44,32 +46,82 @@ impl Bullet {
     fn new(direction: u16, mut position: Position) -> Bullet {
         match direction {
             1 => position.0 -= 1, // x + 1
-            2 => position.1 += 1, // x + 1
-            3 => position.0 += 1, // x - 1
-            4 => position.1 -= 1, // y - 1
+            //2 => position.1 += 1, // x + 1 SAME AS BELOW
+            //3 => position.0 += 1, // x - 1
+            //4 => position.1 -= 1, // y - 1
             _ => ()
         };
-        Bullet {
-            direction,
-            position,
-            speed: 1,     
-        }
+        Bullet { direction, position }
     }
 
     fn update_position(&mut self) {
         match self.direction {
             1 => self.position.0 -= 1, // x + 1
-            2 => self.position.1 += 1, // x + 1
-            3 => self.position.0 += 1, // x - 1
-            4 => self.position.1 -= 1, // y - 1
+            //2 => self.position.1 += 1, // x + 1 TODO: Shoot to the left, right, back
+            //3 => self.position.0 += 1, // x - 1
+            //4 => self.position.1 -= 1, // y - 1
             _ => ()
         };
     }
 }
 
+impl ElementsOnMap {
+    // Update bullets on map and delete these which went past it.
+    fn update_map(&mut self) {
+        for bullet in &mut self.bullets {
+            bullet.update_position()
+        }
+        self.bullets.retain(|x| x.position.0 >= 1);
+    }
+
+    fn display_map(&self, terminal: &mut RawTerminal<Stdout>)
+        -> Result<(), std::io::Error>
+        {
+            let size: u16 = 8;
+            write!(*terminal,
+                   "{}{}  a b c d e f g h\n",
+                   termion::clear::All,
+                   termion::cursor::Goto(1, 1),
+                   )?;
+            for row in 1..=size { 
+                write!(*terminal,
+                       "{}{} ",
+                       termion::cursor::Goto(1, row+1),
+                       row,
+                       )?;
+                for col in 1..=size {
+                    let mut is_bullet = false;
+                    for bullet in &self.bullets {
+                        if bullet.position.get_coordinates() == (row, col) {
+                            is_bullet = true;
+                            break
+                        }
+                    }
+                    if self.player.position.get_coordinates() == (row, col) {
+                        write!(*terminal, "x ")?;
+                    } else if is_bullet {
+                        write!(*terminal, "↥ ")?;
+                    } else {
+                        write!(*terminal, "□ ")?;
+                    }
+                }
+                write!(*terminal, "{}\n", row)?;
+            }
+            write!(*terminal,
+                   "{}  a b c d e f g h\n",
+                   termion::cursor::Goto(1, size+2),
+                   )?;
+            Ok(())
+        }
+
+}
+
 fn main() {
-    let mut map = ElementsOnMap {bullets: Vec::new()};
-    let mut player1 = Player { position:Position(3, 1) };
+    let player = Player { position:Position(3, 1) };
+    let mut map = ElementsOnMap {
+        bullets: Vec::new(),
+        player,
+    };
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
 
@@ -81,67 +133,19 @@ fn main() {
 
     for c in stdin.keys() {
         match c.unwrap() {
-            Key::Up        => player1.move_player(1),
-            Key::Right     => player1.move_player(2),
-            Key::Down      => player1.move_player(3),
-            Key::Left      => player1.move_player(4),
+            Key::Char('q') => panic!("CLICKED q"),
+            Key::Char(' ') => map.bullets.push(Bullet::new(1, map.player.position.clone())),
+            Key::Up        => map.player.move_player(1),
+            Key::Right     => map.player.move_player(2),
+            Key::Down      => map.player.move_player(3),
+            Key::Left      => map.player.move_player(4),
             _              => (),
         };
 
-        map.bullets.push(Bullet::new(1, player1.position.clone()));
-
-        for bullet in map.bullets.iter() {
-            bullet.update_position()
-        }
-
-        display_map(&player1, &mut stdout, &map).unwrap();
+        map.display_map(&mut stdout).expect("ERROR WHILE DISPLAYING");
+        map.update_map();
         stdout.flush().unwrap();
     }
     write!(stdout, "{}", termion::cursor::Show).unwrap();
 }
 
-fn display_map(player: &Player,
-               terminal: &mut RawTerminal<Stdout>,
-               map: &ElementsOnMap
-               )
-    -> Result<(), std::io::Error>
-{
-    let size: u16 = 8;
-    write!(*terminal,
-           "{}{}  a b c d e f g h\n",
-           termion::clear::All,
-           termion::cursor::Goto(1, 1),
-           )?;
-    for row in 1..=size { 
-        write!(*terminal,
-               "{}{} ",
-               termion::cursor::Goto(1, row+1),
-               row,
-               )?;
-        for col in 1..=size {
-            let mut is_bullet = false;
-            for bullet in &map.bullets {
-                if bullet.position.get_coordinates() == (row, col) {
-                    is_bullet = true;
-                    break
-                }
-            }
-            if player.position.get_coordinates() == (row, col) {
-                write!(*terminal, "x ")?;
-            } else if is_bullet {
-                write!(*terminal, "* ")?;
-            } else {
-                write!(*terminal, "□ ")?;
-            }
-        }
-        write!(*terminal, "{}\n", row)?;
-    }
-    write!(*terminal,
-           "{}  a b c d e f g h\n",
-           termion::cursor::Goto(1, size+2),
-           )?;
-    Ok(())
-}
-
-fn shoot() {
-}
